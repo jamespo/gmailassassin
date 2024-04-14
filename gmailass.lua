a6285c034145654468c2ad6f31b6329be2b03ce5
James Powell <jamespo@gmail.com>
2024-04-14 16:23:15 +0100

    print spam senders

--- gmailass.lua
+++ gmailass.lua
@@ -2,15 +2,16 @@ options.certificates = false
 
 dofile(os.getenv("HOME") .. "/.private/imapfilter/gmailcreds.lua")
 
-
 all = gmaccount.INBOX:is_unseen()
 
 spams = Set {}
+
 for _, mesg in ipairs(all) do
     mbox, uid = table.unpack(mesg)
     text = mbox[uid]:fetch_message()
-    if (pipe_to('spamc -E', text) == 1) then
-        table.insert(spams, mesg)
+    if (pipe_to('spamc -c', text) == 1) then
+       print(text:fetch_field('From'))
+       table.insert(spams, mesg)
     end
 end
 
